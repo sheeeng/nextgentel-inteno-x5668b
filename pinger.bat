545bd7297801b8488b030d70b7f4251549cb4336
Leonard Lee <sheeeng@gmail.com>
2014-01-04 21:31:21 +0100

    Add ping to gateway and DNS servers.

--- pinger.bat
+++ pinger.bat
@@ -7,11 +7,11 @@ for /F "usebackq tokens=1,2 delims==" %%i in (`WMIC OS GET LocalDateTime /VALUE 
 :set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2% %ldt:~8,2%:%ldt:~10,2%:%ldt:~12,6%
 set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%_%ldt:~8,2%-%ldt:~10,2%-%ldt:~12,6%
 
-for /f "tokens=1,2 delims=." %%a in ("%ldt%") do set datetime=%%a&set deciseconds=%%b
+for /f "tokens=1,2 delims=." %%a in ("%ldt%") do set datetime=%%a&set milliseconds=%%b
 echo.DateTime: %datetime%
-echo.Deciseconds: %deciseconds%
+echo.Milliseconds: %milliseconds%
 
-set outputFile=%CD%\logs\pinger_%datetime%_%deciseconds%.log
+set outputFile=%CD%\logs\pinger_%datetime%_%milliseconds%.log
 echo %outputFile%
 
 :DOS - String Manipulation
@@ -24,7 +24,13 @@ echo ------------------------------------------------------------------------ >>
 echo.
 echo Local date is [%ldt%]. >> %outputFile% 2>&1
 echo.
-ping -n 4 www.nextgentel.no >> %outputFile% 2>&1
+:NextGenTel
+ping -n 2 www.nextgentel.no >> %outputFile% 2>&1
+:Gateway
+ping -n 2 80.203.47.1 >> %outputFile% 2>&1
+:DNS Servers
+ping -n 2 217.13.7.140 >> %outputFile% 2>&1
+ping -n 2 217.13.4.24 >> %outputFile% 2>&1
 echo ------------------------------------------------------------------------ >> %outputFile% 2>&1
 
-GrabAdslRates.vbs
+grab_adsl_rates.vbs
--- pinger.ps1
+++ pinger.ps1
@@ -11,10 +11,16 @@ Write-Output $unixDateTime | Out-File -Append $outputFile 2>&1
 Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
 Write-Host "Local date (`$unixDateTime) is [$unixDateTime]." | Tee-Object $outputFile 2>&1
 Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
-ping -n 4 www.nextgentel.no | Tee-Object $outputFile 2>&1
+#NextGenTel
+ping -n 2 www.nextgentel.no | Tee-Object $outputFile 2>&1
+#Gateway
+ping -n 2 80.203.47.1  | Tee-Object $outputFile 2>&1
+#DNS Servers
+ping -n 2 217.13.7.140  | Tee-Object $outputFile 2>&1
+ping -n 2 217.13.4.24  | Tee-Object $outputFile 2>&1
 Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
 
 #Telnet Automation with PowerShell made simple… | Max Trinidad – The PowerShell Front
 #http://www.maxtblog.com/2012/06/telnet-automation-with-powershell-made-simple/
 
-.\GrabAdslRates.vbs
+.\grab_adsl_rates.vbs
