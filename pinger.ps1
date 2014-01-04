b1dc11a1e47e73c2e53fabc6a10643a3376e6aaf
Leonard Lee <sheeeng@gmail.com>
2014-01-04 22:29:28 +0100

    Change console buffer and windows size.
    Workaround for Windows Powershell that truncated Test-Connection
    output.

--- pinger.ps1
+++ pinger.ps1
@@ -1,4 +1,10 @@
-﻿#Get-Date
+﻿#How Can I Expand the Width of the Windows PowerShell Console? - Hey, Scripting Guy! Blog - Site Home - TechNet Blogs
+#http://blogs.technet.com/b/heyscriptingguy/archive/2006/12/04/how-can-i-expand-the-width-of-the-windows-powershell-console.aspx
+
+$host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(175,20000)
+$host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size(160,60)
+
+#Get-Date
 #http://technet.microsoft.com/en-us/library/hh849887.aspx
 
 Write-Host "The value of the `$pshome variable is $pshome."
@@ -6,19 +12,23 @@ $unixDateTime = Get-Date -UFormat %s
 $unixDateTime = $unixDateTime.replace('.','_') 
 
 $outputFile = Join-Path ($PWD) logs/pinger_$unixDateTime.log
-Write-Host $outputFile
-Write-Output $unixDateTime | Out-File -Append $outputFile 2>&1
-Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
-Write-Host "Local date (`$unixDateTime) is [$unixDateTime]." | Tee-Object $outputFile 2>&1
-Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
+Write-Host The `$outFile has $outputFile file path.
+#Write-Output $unixDateTime | Out-File $outputFile 2>&1
+Write-Output "------------------------------------------------------------------------" | Tee-Object -Append $outputFile 2>&1
+Write-Output "Local date (`$unixDateTime) is [$unixDateTime]." | Tee-Object -Append $outputFile 2>&1
+Write-Output "------------------------------------------------------------------------" | Tee-Object -Append $outputFile 2>&1
 #NextGenTel
-ping -n 2 www.nextgentel.no | Tee-Object $outputFile 2>&1
+Test-Connection -Count 2 -Verbose www.nextgentel.no | Tee-Object -Append -FilePath $outputFile 2>&1
+ping -n 2 www.nextgentel.no | Tee-Object -Append -FilePath $outputFile 2>&1
 #Gateway
-ping -n 2 80.203.47.1  | Tee-Object $outputFile 2>&1
+Test-Connection -Count 2 -Verbose 80.203.47.1 | Tee-Object -Append -FilePath $outputFile 2>&1
+ping -n 2 80.203.47.1 | Tee-Object -Append -FilePath $outputFile 2>&1
 #DNS Servers
-ping -n 2 217.13.7.140  | Tee-Object $outputFile 2>&1
-ping -n 2 217.13.4.24  | Tee-Object $outputFile 2>&1
-Write-Host  "------------------------------------------------------------------------" | Tee-Object $outputFile 2>&1
+Test-Connection -Count 2 -Verbose 217.13.7.140 | Tee-Object -Append -FilePath $outputFile 2>&1
+Test-Connection -Count 2 -Verbose 217.13.4.24 | Tee-Object -Append -FilePath $outputFile 2>&1
+ping -n 2 217.13.7.140 | Tee-Object -Append -FilePath $outputFile 2>&1
+ping -n 2 217.13.4.24 | Tee-Object -Append -FilePath $outputFile 2>&1
+Write-Host "------------------------------------------------------------------------" | Tee-Object -Append -FilePath $outputFile 2>&1
 
 #Telnet Automation with PowerShell made simple… | Max Trinidad – The PowerShell Front
 #http://www.maxtblog.com/2012/06/telnet-automation-with-powershell-made-simple/
